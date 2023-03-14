1df2d303273955f94d8731205f2a41a889b68a54
AndrewJJack <andrew@dakotaanalytics.com>
2023-03-14 10:15:30 -0600

    updated

--- Terraform/.terraform.lock.hcl
+++ Terraform/.terraform.lock.hcl
@@ -1,21 +1,40 @@
 # This file is maintained automatically by "terraform init".
 # Manual edits may be lost in future updates.
 
-provider "registry.terraform.io/hashicorp/azurerm" {
-  version = "3.46.0"
+provider "registry.terraform.io/hashicorp/azuread" {
+  version     = "2.19.1"
+  constraints = "2.19.1"
   hashes = [
-    "h1:FXfv07ZZIjm08jWL1Ykd69WJkm6VxS+WeSfqNIWtI0U=",
-    "zh:02327ad31c998d9d55bafd280a4b1bd3702b496e49f53fad30823712cc85368a",
-    "zh:05882df6b5b59d23e2aa0c454caea84fcc35e435eb925e18293415260b4850c8",
-    "zh:0c0d6309abcfa24f0775df9bff81cb8f63029fead66956316d7cfa837c231873",
-    "zh:3607e8f5da720e053f7bada6b7c21b018ba25ccff8dade66bb64c2544214b400",
-    "zh:5662a2910bda19242b7be6bd7582a514082309691c97ee97aa9cf45bbbf45af4",
-    "zh:58d6de82cf26e2764f3d57d0e032b06cc97c90756044486b206d83cea53d3f7a",
-    "zh:5ee2f224ac1185e44fc4a2d60deb5c4a162d0fda07516e0939520cb8dcbedaca",
-    "zh:bbb6f9fd9cb6e8a79c9e1e3ce74647f743f60f3ea2c325b9aececf4a55ec0343",
-    "zh:c9d923b415f411eb68dfd43fab1753087b4701caeceb94550df562ca417c6007",
-    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
-    "zh:f6157b462ed33c27f101113b46463130d00de7f2731e05a371c922e5730a4b5e",
-    "zh:f96be10c899c0e30870056ec9c2c5002ec2e4f07fe2a64e1b3135ec232ef6f1e",
+    "h1:BSHkkfcq4v1gwOamAn4GCqbXwduxkEn6H2KknoeosHM=",
+    "zh:03efff4c5b7ce6feb46562bcdcf6dcdf7b6d60942d9c35cf48b72d1e927b20d7",
+    "zh:0eab37bb4d68748bf4365e31a3e204508ea3a52d929b8a099efa38ba4d0eb95e",
+    "zh:5329363f8b862b6fdb3567ec970021b726bfd850b01576f10cf8b4965014f08a",
+    "zh:53dcdab7c6dca1024093cbaf4f9a5fc00e8b16eab90c69961f7c552ed88275cc",
+    "zh:5e23996cb9addbffb4e3b329bb46e0f6efb994f8885ad1d256c1c54576050dea",
+    "zh:625767aa47cad656c04dc72ff151a715642815a76757a41863e135326e0a1ccb",
+    "zh:62ac142b6a246ce7d5dd4fe93374a570652b00ffbc6e29db645b23596f8f9cbe",
+    "zh:66c3b318594bcad52255d66b554df811b5bf589d805df8d24c60f9329584816a",
+    "zh:9347b9d24f636ca39dbaee165882e2158a50ca8795b3e6ed876510ab99fda21f",
+    "zh:9a81755f5ab81147645fe9601a9d84500d295a3f15fcec07d15a7cdccf891ec3",
+    "zh:ab1bd1269e26fd08ba2c17dab100b1fef1f7e49663d86e883d20babd16c8a2df",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "3.0.2"
+  constraints = "3.0.2"
+  hashes = [
+    "h1:QPPDLJTDEVDkBU1HOuXYtKnFULKoOOJeDnUb1GmS8bE=",
+    "zh:4d7dcf1318f23202a33ac1e7dbed692ec80d86813fc50149bd46863e6c9584eb",
+    "zh:64171745d17f10d9cc60ae77443b5439a84fb3cc69d164f3ffe527c5f271e6da",
+    "zh:7bb19cbb5702b19ae010af3f8b8c949b183748e1859165e7268d00ea62f4e460",
+    "zh:7f6e634e9d9c5b43a4ff36f8797f51f00fa13df36361a80f3ca7482349279963",
+    "zh:84f64143e66f627b035fef9345271059d3ce51fea2eb4c52188647ef01124a82",
+    "zh:89ce9fc1a4ec40a0904b68a24fe1e598a4a89d42fc91772c89de57a88c862890",
+    "zh:96b610037db94ae23e7f73ba3296164d761319b7943fc7443f63029e3db7cc9b",
+    "zh:a216690bf0d33b43580ba80088dcc4cb3b39fa0c8eb0e9d221d9f02ca39df41b",
+    "zh:b4f39b1b37d9b29230e36c00e825f279337b2bb9e53b3d3b1ee0544782794606",
+    "zh:b686d9615d2627e1631e6cc07b1a61d9865ea43db8042ea5365fcc9ac5b1f465",
+    "zh:bc96d3bc3d122f81fea271bba5104ba32860d5e84f03da19751e5e8cb883ebef",
   ]
 }
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,107 +1,22 @@
-locals {
-  suffix = length(var.suffix) == 0 ? "" : "-${var.suffix}"
-}
-
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_databricks_workspace" "this" {
-  name                                  = "dbw-${var.project}-${var.env}-${var.location}${local.suffix}"
-  resource_group_name                   = var.resource_group
-  location                              = var.location
-  managed_resource_group_name           = "${var.resource_group}-databricks"
-  sku                                   = var.sku
-  public_network_access_enabled         = var.public_network_access_enabled
-  network_security_group_rules_required = var.nsg_rules_required
-  tags                                  = var.tags
-  managed_services_cmk_key_vault_key_id = var.sku == "premium" && var.customer_managed_service_key_enabled ? azurerm_key_vault_key.databricks_ws_service[0].id : null
-
-  custom_parameters {
-    no_public_ip                                         = var.no_public_ip
-    virtual_network_id                                   = var.network_id
-    public_subnet_name                                   = var.public_subnet_name
-    private_subnet_name                                  = var.private_subnet_name
-    public_subnet_network_security_group_association_id  = var.public_subnet_nsg_association_id
-    private_subnet_network_security_group_association_id = var.private_subnet_nsg_association_id
-  }
-  depends_on = [azurerm_key_vault_access_policy.databricks_ws_service]
-}
-
-resource "azurerm_databricks_access_connector" "this" {
-  count = var.access_connector_enabled ? 1 : 0
-
-  name                = "ac-${var.project}-${var.env}-${var.location}${local.suffix}"
-  resource_group_name = var.resource_group
-  location            = var.location
-  tags                = var.tags
-
-  identity {
-    type = "SystemAssigned"
-  }
-}
-
-data "azurerm_monitor_diagnostic_categories" "this" {
-  for_each = var.sku == "premium" ? var.log_analytics_workspace : {}
-
-  resource_id = azurerm_databricks_workspace.this.id
-}
-
-resource "azurerm_monitor_diagnostic_setting" "this" {
-  for_each = var.sku == "premium" ? var.log_analytics_workspace : {}
-
-  name                           = "monitoring-${var.project}-${var.env}-${var.location}${local.suffix}"
-  target_resource_id             = azurerm_databricks_workspace.this.id
-  log_analytics_workspace_id     = each.value
-  log_analytics_destination_type = var.analytics_destination_type
-
-  dynamic "enabled_log" {
-    for_each = data.azurerm_monitor_diagnostic_categories.this[each.key].log_category_types
-    content {
-      category = enabled_log.value
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.0.2" #without specifying, latest version will be used, may break code
     }
-  }
-
-  lifecycle {
-    ignore_changes = [log_analytics_destination_type] # TODO remove when issue is fixed: https://github.com/Azure/azure-rest-api-specs/issues/9281
-  }
-}
-
-resource "azurerm_key_vault_key" "databricks_ws_service" {
-  count = var.sku == "premium" && var.customer_managed_service_key_enabled ? 1 : 0
-
-  name         = "databricks-managed-services-cmk"
-  key_vault_id = var.key_vault_id
-  key_type     = "RSA"
-  key_size     = 2048
-
-  key_opts = [
-    "decrypt",
-    "encrypt",
-    "sign",
-    "unwrapKey",
-    "verify",
-    "wrapKey",
-  ]
-}
-
-resource "azurerm_key_vault_access_policy" "databricks_ws_service" {
-  count = var.sku == "premium" && var.customer_managed_service_key_enabled ? 1 : 0
-
-  key_vault_id = var.key_vault_id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = var.global_databricks_object_id
-  key_permissions = [
-    "Get",
-    "List",
-    "Encrypt",
-    "Decrypt",
-    "WrapKey",
-    "UnwrapKey"
-  ]
-
-  lifecycle {
-    precondition {
-      condition     = alltrue([var.customer_managed_service_key_enabled, var.key_vault_id != null])
-      error_message = "To encrypt Databricks Workspace Services, please provide for key_vault_id variable, which points to Key Vault, where CMK key would be created"
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "2.19.1"
     }
   }
 }
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "AJTerraformResourceGroup"
+  location = "Canada Central"
+}
+
